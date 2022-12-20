e57bbfdef4f20aeb526b3b102696fd1671055102
Nikita Ilyanov <nikita.glk@mail.ru>
2022-12-20 14:23:55 +0300

    feat: add solutions to numbers tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -182,8 +182,9 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const rank = pow === 0 ? 1 : 10 ** pow;
+  return Math.round(num / rank) * rank;
 }
 
 /**
@@ -203,8 +204,11 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) return false;
+  }
+  return n > 1;
 }
 
 /**
@@ -222,8 +226,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return Number(value) ? Number(value) : def;
 }
 
 module.exports = {
